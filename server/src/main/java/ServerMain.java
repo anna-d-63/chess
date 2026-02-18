561e156d9c0c996771561f42ae19ecca14e76dfa
Anna <dixonanna000@gmail.com>
2026-02-18 11:20:21 -0700

    Did the getting started tasks and created UserData, GameData, and AuthData records.

---
+++ server/src/main/java/ServerMain.java
@@ -0,0 +1,20 @@
+//import chess.*;
+//
+//public class ServerMain {
+//    public static void main(String[] args) {
+//        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+//        System.out.println("♕ 240 Chess Server: " + piece);
+//    }
+//}
+
+
+import server.Server;
+
+public class ServerMain {
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.run(8080);
+
+        System.out.println("♕ 240 Chess Server");
+    }
+}
