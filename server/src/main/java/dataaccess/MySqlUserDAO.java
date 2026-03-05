a7cf6bd9c9956f0a62a3cb310c658794f266aca6
Anna <dixonanna000@gmail.com>
2026-03-05 15:28:07 -0700

    started building out MySqlUserDAO, should be able to create the users table and create database if does not exist.

--- server/src/main/java/dataaccess/MySqlUserDAO.java
+++ server/src/main/java/dataaccess/MySqlUserDAO.java
@@ -2,6 +2,10 @@ package dataaccess;
 
 import model.UserData;
 
+import javax.xml.crypto.Data;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class MySqlUserDAO implements UserDAO {
 
     @Override
@@ -34,5 +38,31 @@ public class MySqlUserDAO implements UserDAO {
     @Override
     public String toString() {
         return super.toString();
+    }
+
+    private final String[] createUserStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS users (
+              `id` int NOT NULL AUTO_INCREMENT,
+              `username` varchar(256) NOT NULL,
+              `password` varchar(256) NOT NULL,
+              `email` varchar(256) NOT NULL,
+              PRIMARY KEY (`id`),
+              INDEX(username)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+    private void configureDatabase() throws Exception {
+        DatabaseManager.createDatabase();
+        try (Connection conn = DatabaseManager.getConnection()) {
+            for (String statement : createUserStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new Exception(ex.getMessage());
+        }
     }
 }
