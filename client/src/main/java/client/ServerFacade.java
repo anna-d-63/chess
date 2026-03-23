c91248f1cfce3af59bdb48d9efe0273c21b537fb
Anna <dixonanna000@gmail.com>
2026-03-23 13:06:48 -0600

    made the server facade, methods and junit tests for register, login, and logout

---
+++ client/src/main/java/client/ServerFacade.java
@@ -0,0 +1,92 @@
+package client;
+
+import com.google.gson.Gson;
+import dataaccess.DataAccessException;
+import server.requestandresult.*;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class ServerFacade {
+    private final HttpClient client = HttpClient.newHttpClient();
+    private final int port;
+    private final Gson serializer = new Gson();
+
+    public ServerFacade(int port) {this.port = port;}
+
+    public RegisterResult register(RegisterRequest registerRequest) throws DataAccessException {
+        var req = buildRequest("POST", "/user", registerRequest, null);
+        var response = sendRequest(req);
+        return handleResponse(response, RegisterResult.class);
+    }
+
+    public LoginResult login(LoginRequest loginRequest) throws DataAccessException {
+        var req = buildRequest("POST", "/session", loginRequest, null);
+        var response = sendRequest(req);
+        return handleResponse(response, LoginResult.class);
+    }
+
+    public void logout(LogoutRequest logoutRequest, String authToken) throws DataAccessException {
+        var req = buildRequest("DELETE", "/session", logoutRequest, authToken);
+        var response = sendRequest(req);
+        handleResponse(response, null);
+    }
+
+    public void clear() throws DataAccessException {
+        var request = buildRequest("DELETE", "/db", null, null);
+        sendRequest(request);
+    }
+
+    private HttpRequest buildRequest(String method, String path, Object body, String authToken) {
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:" + port + path))
+                .method(method, makeRequestBody(body));
+        if (authToken != null) {
+            request.setHeader("Authorization", authToken);
+        }
+        return request.build();
+    }
+
+    private BodyPublisher makeRequestBody(Object request) {
+        if (request != null) {
+            return BodyPublishers.ofString(serializer.toJson(request));
+        } else {
+            return BodyPublishers.noBody();
+        }
+    }
+
+    private HttpResponse<String> sendRequest(HttpRequest request) throws DataAccessException {
+        try {
+            return client.send(request, BodyHandlers.ofString());
+        } catch (Exception e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+    private <T> T handleResponse(HttpResponse<String> response, Class<T> responseClass) throws DataAccessException {
+        var status = response.statusCode();
+        if (!isSuccessful(status)) {
+            var body = response.body();
+            throw new DataAccessException(body.toString());
+        }
+        if (responseClass != null) {
+            return serializer.fromJson(response.body(), responseClass);
+        }
+        return null;
+    }
+
+    private boolean isSuccessful(int status) {
+        return status/100 == 2;
+    }
+
+//    public CreateGameResult createGame(CreateGameRequest request) {}
+//
+//    public void joinGame(JoinGameRequest request) {}
+//
+//    public ListGamesResult listGames(ListGamesRequest request) {}
+}
--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,32 @@
 package client;
 
+import dataaccess.DataAccessException;
 import org.junit.jupiter.api.*;
 import server.Server;
+import server.requestandresult.*;
+
+import javax.xml.crypto.Data;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade(port);
+    }
+
+    @BeforeEach
+    public void clearDB() throws DataAccessException {
+        facade.clear();
     }
 
     @AfterAll
@@ -22,8 +36,41 @@ public class ServerFacadeTests {
 
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void register() throws DataAccessException {
+        var request = new RegisterRequest("Anna", "password", "anna@email.com");
+        RegisterResult res = facade.register(request);
+
+        assertNotNull(res);
+        assertEquals("Anna", res.username());
+        assertNotNull(res.authToken());
+    }
+
+    @Test
+    public void logout() throws DataAccessException {
+        var request = new RegisterRequest("Anna", "password", "anna@email.com");
+        RegisterResult result = facade.register(request);
+
+        assertNotNull(result);
+        assertEquals("Anna", result.username());
+        assertNotNull(result.authToken());
+
+        var logoutRequest = new LogoutRequest(result.authToken());
+        facade.logout(logoutRequest, result.authToken());
+    }
+
+    @Test
+    public void login() throws DataAccessException {
+        var registerRequest = new RegisterRequest("Anna", "pwd", "anna@email.com");
+        RegisterResult registerResult = facade.register(registerRequest);
+
+        var logoutRequest = new LogoutRequest(registerResult.authToken());
+        facade.logout(logoutRequest, registerResult.authToken());
+
+        var loginRequest = new LoginRequest("Anna", "pwd");
+        LoginResult loginResult = facade.login(loginRequest);
+
+        assertEquals("Anna", loginResult.username());
+        assertNotNull(loginResult.authToken());
     }
 
 }
